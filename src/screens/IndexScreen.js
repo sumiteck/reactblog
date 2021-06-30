a118f80327f40d769a6fcc290d3a2256c7baf2ac
sumitk68 <imsumitk1@gmail.com>
2021-06-29 22:53:46 -0400

    index Screen added

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const IndexScreen = () => {
+return (
+    <View>
+        <Text>Index Screen</Text>
+    </View>
+    
+);
+
+};
+
+const Styles = StyleSheet.create({});
+
+export default IndexScreen;
