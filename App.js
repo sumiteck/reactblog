fbac67c7e4009117be4a556c2242402ff98174df
sumitk68 <imsumitk1@gmail.com>
2021-07-01 19:30:37 -0400

    Context created

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator, createAppContainer } from "react-navigation";
+import IndexScreen from "./src/screens/IndexScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen
   },
-});
+  {
+    initialRouteName: "Index",
+    defaultNaviationOption: {
+      title: "Blogs"
+    }
+  }
+);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />
+};
